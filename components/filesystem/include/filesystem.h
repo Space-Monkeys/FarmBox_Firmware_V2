2ef89a0db81c91ac0998fc89e090a249a74b53c9
Kelviny Henrique <kelvinyhenrique17@gmail.com>
2021-10-10 22:30:57 -0300

    Removendo const

--- components/filesystem/include/filesystem.h
+++ components/filesystem/include/filesystem.h
@@ -1,5 +1,5 @@
 const char *getFiles(char *filepath);
 void filesystem_init();
-const char *readFile(char *filepath);
+char *readFile(char *filepath);
 void writeFile(char *filepath, char *file_data);
 void editFile(char *filepath, char *json_key, char *json_key_data);
