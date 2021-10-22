bf094278a0cfb31cf730e5a0483fcdbb964aafbc
kelvinyhenrique <kelvinyhenrique17@gmail.com>
2021-10-22 14:43:48 -0300

    Configure dinamic pump config by file for change into app

--- components/water_pump/water_pump.c
+++ components/water_pump/water_pump.c
@@ -7,18 +7,34 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "time.h"
+#include <string.h>
 
 static const char *WATER_PUMP = "WATER_PUMP";
 #define PUMP_GPIO 18
 
 void pump_actions()
 {
-    int duration = 10000;
+    cJSON *pump_config_t = NULL;
+    int pump_activation_interval = 0;
+    char *pump_config_json = NULL;
+    cJSON *pump_config_str_interval = NULL;
+    pump_config_json = readFile("/spiffs/pump/config.json");
+    pump_config_t = cJSON_Parse(pump_config_json);
+
+    pump_config_str_interval = cJSON_GetObjectItemCaseSensitive(pump_config_t, "interval");
+
+    if (cJSON_IsString(pump_config_str_interval) && (pump_config_str_interval->valuestring != NULL))
+    {
+        pump_config_json = pump_config_str_interval->valuestring;
+        pump_activation_interval = atoi(pump_config_json);
+    }
+    printf("%d", pump_activation_interval);
+
     ESP_LOGI(WATER_PUMP, "Setting gpio of pump...");
     gpio_pad_select_gpio(PUMP_GPIO);
     gpio_set_direction(PUMP_GPIO, GPIO_MODE_OUTPUT);
     activate_pump(1, PUMP_GPIO);
-    vTaskDelay(duration / portTICK_PERIOD_MS);
+    vTaskDelay(pump_activation_interval / portTICK_PERIOD_MS);
     activate_pump(0, PUMP_GPIO);
 }
 
