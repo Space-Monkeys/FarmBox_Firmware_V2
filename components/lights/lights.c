2935a0f623e81359e3f1fbe0ab86b636ae18b4c7
kelvinyhenrique <kelvinyhenrique17@gmail.com>
2021-10-22 18:39:44 -0300

    Add response if have error in write file

--- components/lights/lights.c
+++ components/lights/lights.c
@@ -16,8 +16,10 @@ void light_actions(void)
 
     cJSON *lights_config_t = NULL;
     int lights_is_active = 0;
+    int resfile = 0;
     char *lights_config_json = NULL;
     cJSON *lights_status = NULL;
+    char *new_test_var = NULL;
     lights_config_json = readFile("/spiffs/lights/config.json");
     lights_config_t = cJSON_Parse(lights_config_json);
 
@@ -26,22 +28,30 @@ void light_actions(void)
     if (cJSON_IsString(lights_status) && (lights_status->valuestring != NULL))
     {
         lights_config_json = lights_status->valuestring;
-        printf("Before conversion: %s \n", lights_config_json);
         lights_is_active = atoi(lights_config_json);
-        printf("After conversion: %d \n", lights_is_active);
     }
 
     if (lights_is_active == 0)
     {
         lights_status->valuestring = "1";
         lights_config_json = cJSON_Print(lights_config_t);
-        writeFile("/spiffs/lights/config.json", lights_config_json);
+        resfile = writeFile("/spiffs/lights/config.json", lights_config_json);
         ESP_LOGI(LIGHTS, "The lights are on");
     }
     else
     {
         lights_config_json = lights_status->valuestring = "0";
+        lights_config_json = cJSON_Print(lights_config_t);
         ESP_LOGI(LIGHTS, "The lights are Off");
-        writeFile("/spiffs/lights/config.json", lights_config_json);
+        resfile = writeFile("/spiffs/lights/config.json", lights_config_json);
+    }
+    if (resfile == 1)
+    {
+        lights_config_t = NULL;
+        lights_config_json = NULL;
+    }
+    else
+    {
+        ESP_LOGE(LIGHTS, "Error to writeFile");
     }
 }
